2ba131508284ed05f11248e5366fdc15531959b0
Rachit Jain <rachitjain9596@gmail.com>
2024-02-05 23:53:51 +0530

    StarRating component p5-adding customization props

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <StarRating size={24} />
     <StarRating maxRating={10} />
   </React.StrictMode>
 );
