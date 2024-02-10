4f2a26db2c3ca5a577e972bc01a73b581258d09d
Rachit Jain <rachitjain9596@gmail.com>
2024-02-10 13:36:39 +0530

    App-v1 complete

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 import StarRating from './StarRating';
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie has {movieRating} stars</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie has {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       size={24}
       messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
       onSetRating={null}
     />
     <StarRating maxRating={10} defaultRating={3} onSetRating={null} />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
